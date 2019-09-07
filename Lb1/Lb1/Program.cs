d34d82880edbbdb68c9d5cfdb75d412f20f1c737
NikolayKyryllov <54995168+NikolayKyryllov@users.noreply.github.com>
2019-09-07 22:42:59 +0300

    Add files via upload

---
+++ Lb1/Lb1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double v, t, h;
+            Console.Write("enter starting speed: ");
+            v = double.Parse(Console.ReadLine());
+            Console.Write("enter height: ");
+            h = double.Parse(Console.ReadLine());
+            t = 2 * h / v;
+            Console.WriteLine($"t={t}");
+            Console.ReadKey();
+        }
+    }
+}
